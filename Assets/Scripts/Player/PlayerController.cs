190990bdff885a7d0871ccf0b3771497d25dc73f
DavidR1997 <david.rowsellwork@gmail.com>
2018-09-27 17:12:43 +1000

    Initial commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+/*
+ * The PlayerController handles player movement & rotation logic
+ */
+public class PlayerController : MonoBehaviour
+{
+    public NavMeshAgent agent; // reference to NavMeshAgent component
+
+    //Calls this to tell agent where to go.
+    public void SetDestination(Vector3 target)
+    {
+        //NavMesh path creates a new instances of path to contain the new path
+        NavMeshPath path = new NavMeshPath();
+        //Calculates the new path to the target
+        if (agent.CalculatePath(target, path))
+        {
+            //if the path generates is completed
+            if (path.status == NavMeshPathStatus.PathComplete)
+            {
+                //Agent goes to the target destination
+                agent.SetDestination(target);
+            }
+        }
+    }
+}
